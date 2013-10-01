1ddbb0d717919e5b1ea87d1f896c134fb52fdf2d
Tehmasp Chaudhri <tehmasp@gmail.com>
2013-10-01 14:37:52 -0600

    Removed extra dash in between the TODO tasks

--- TODO.rb
+++ TODO.rb
@@ -1,5 +1,5 @@
-#TODO - Add Vagrant support.
-#TODO - Add full rspec-puppet and serverspec support.
-#TODO - Add Stash API / git push support.
-#TODO - Add a configuration file option to Magnum to allow more templating functionality.
-#TODO - Polish up and document a usage tutorial.
+#TODO Add Vagrant support.
+#TODO Add full rspec-puppet and serverspec support.
+#TODO Add Stash API / git push support.
+#TODO Add a configuration file option to Magnum to allow more templating functionality.
+#TODO Polish up and document a usage tutorial.
