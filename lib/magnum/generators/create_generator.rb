d1c61a5bac0d38bb7a4a2299a710998e3907f6ff
Tehmasp Chaudhri <tehmasp@gmail.com>
2013-10-08 15:00:15 -0600

    Adding proper rspec-puppet, serverspec, and Vagrant support to Magnum; Updated the Rakefile to run the spec tests properly

--- lib/magnum/generators/create_generator.rb
+++ lib/magnum/generators/create_generator.rb
@@ -5,83 +5,93 @@ module Magnum
       type: :string,
       required: true
 
+    class_option :license,
+      type: :string,
+      default: 'reserved'
+
     class_option :maintainer,
       type: :string,
       default: 'Pearson, Inc.'
-
-    class_option :copyright_holder,
-      type: :string
 
     class_option :maintainer_email,
       type: :string,
       default: 'archautomation@pearson.com'
 
-    class_option :license,
-      type: :string,
-      default: 'reserved'
+    class_option :copyright_holder,
+      type: :string
 
     def write_emptydirs
-      empty_directory target.join('spec')
       empty_directory target.join('files')
       empty_directory target.join('manifests')
       empty_directory target.join('templates')
-    end
-
-    def write_license
-      template license_file, target.join('LICENSE')
+      empty_directory target.join('spec')
+      empty_directory target.join('serverspec/spec')
     end
 
     def write_readme
       template 'README.md.erb', target.join('README.md')
     end
 
-    def write_gemfile
-      template 'Gemfile.erb', target.join('Gemfile')
+    def write_license
+      template license_file, target.join('LICENSE')
     end
 
-    def write_modulefile
-      template 'ModuleFile.erb', target.join('ModuleFile')
+    def write_gemfile
+      template 'util/Gemfile.erb', target.join('Gemfile')
     end
 
     def write_rakefile
-      template 'Rakefile.erb', target.join('Rakefile')
+      template 'util/Rakefile.erb', target.join('Rakefile')
+    end
+
+    def write_modulefile
+      template 'puppet/ModuleFile.erb', target.join('ModuleFile')
     end
 
     def write_manifests
-      template 'init.pp.erb', target.join('manifests/init.pp')
+      template 'puppet/init.pp.erb', target.join('manifests/init.pp')
     end
 
-    def write_git_setup
-      template 'gitignore.erb', target.join('.gitignore')
-
-      unless File.exists?(target.join('.git'))
-        inside target do
-          run 'git init', capture: true
-
-          run 'git add -A', capture: true
-          run "git commit -m 'initial commit. Puppet module created by Magnum'", capture: true
-
-          say "\n\tNOTE: Remember to setup a git repo and a remote to push your changes to.\n", :red
-        end
+    def write_vagrantfile
+      unless which('vagrant')
+        say "\n\tNOTE: Vagrant does NOT appear to be installed." +
+            "\n\tPlease install it from http://www.vagrantup.com/", :red
       end
 
-      template 'puppet-git-hooks-pre-commit.erb', target.join('.git/hooks/pre-commit')
-      chmod target.join('.git/hooks/pre-commit'), 0755
+      say "\n\tNOTE: Installing Vagrantfile with 'default' box.\n", :green
+      template 'vagrant/Vagrantfile.erb', target.join('Vagrantfile')
     end
 
     def write_spec_setup
       inside target do
-        run 'rspec-puppet-init &>/dev/null'
+        # use rspec-puppet-init to create spec folder layout
+        run 'rspec-puppet-init &>/dev/null', capture: true
       end
 
-      # remove rspec-puppet only spec_helper.rb
+      # for now we are controlling the spec_helper.rb file for rspec-puppet testing
       remove_file target.join('spec/spec_helper.rb')
+      template 'spec/rspec/spec_helper.rb.erb', target.join('spec/spec_helper.rb')
+      template 'spec/rspec/init_spec.rb.erb', target.join("spec/classes/#{module_name}_spec.rb")
 
-      # write our rspec-puppet / serverspec combined spec_helper
-      template 'spec_helper.rb.erb', target.join('spec/spec_helper.rb')
+      template 'spec/serverspec/spec_helper.rb.erb', target.join('serverspec/spec_helper.rb')
+      template 'spec/serverspec/init_spec.rb.erb', target.join("serverspec/spec/#{module_name}_spec.rb")
+    end
 
-      # create default serverspec directory
-      empty_directory target.join('spec/serverspec')
+    def write_git_setup
+      template 'git/gitignore.erb', target.join('.gitignore')
+
+      unless File.exists?(target.join('.git'))
+        inside target do
+          run 'git init', capture: true
+          run 'git add -A', capture: true
+          run "git commit -m 'Initial commit. Puppet module created by Magnum'", capture: true
+
+          say "\n\tNOTE: Remember to setup a git repo and a remote to push your changes to.\n", :green
+        end
+      end
+
+      template 'git/puppet-git-hooks-pre-commit.erb', target.join('.git/hooks/pre-commit')
+      chmod target.join('.git/hooks/pre-commit'), 0755
     end
 
     private
@@ -94,7 +104,7 @@ module Magnum
       case options[:license]
       when 'apachev2'; 'Apache 2.0'
       when 'mit'; 'MIT'
-      when 'reserved'; 'All rights reserved'
+      when 'reserved'; 'All Rights Reserved'
       else
         raise "Unknown license: '#{options[:license]}'"
       end
@@ -114,12 +124,15 @@ module Magnum
       end
     end
 
-    def copyright_year
-      Time.now.year
-    end
-
-    def copyright_holder
-      options[:copyright_holder] || maintainer
+    def which(cmd)
+      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+        exts.each { |ext|
+          exe = File.join(path, "#{cmd}#{ext}")
+          return exe if File.executable? exe
+        }
+      end
+      return nil
     end
 
     def maintainer
@@ -128,6 +141,14 @@ module Magnum
 
     def maintainer_email
       options[:maintainer_email]
+    end
+
+    def copyright_year
+      Time.now.year
+    end
+
+    def copyright_holder
+      options[:copyright_holder] || maintainer
     end
 
     def default_options
