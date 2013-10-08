b80f2624a95d95db51ba18d1f1e566b7903ddbe1
Tehmasp Chaudhri <tehmasp@gmail.com>
2013-10-08 14:55:07 -0600

    Commented out the init subcommand from Magnum for the time being

--- lib/magnum/cli/module.rb
+++ lib/magnum/cli/module.rb
@@ -6,10 +6,10 @@ module Magnum
       Magnum::CreateGenerator.new([File.join(Dir.pwd, module_name), module_name], options).invoke_all
     end
 
-    desc "init [MODULE_PATH]", "Initializes an existing Puppet module."
-    def init(module_path)
-      Magnum::InitGenerator.new([File.join(Dir.pwd, module_path), module_path], options).invoke_all
-    end
+    # desc "init [MODULE_PATH]", "Initializes an existing Puppet module."
+    # def init(module_path)
+    #   Magnum::InitGenerator.new([File.join(Dir.pwd, module_path), module_path], options).invoke_all
+    # end
 
     def self.banner(task, namespace = false, subcommand = true)
       "#{basename} #{task.formatted_usage(self, namespace, subcommand).split(':').join(' ')}"
